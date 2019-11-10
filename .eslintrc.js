1a3cd199cf28061e87ef3d390b71de43bce05503
Martin Rosenberg <Martin.B.Rosenberg@gmail.com>
2019-11-10 10:56:51 -0700

    Switch Prettier/TSLint to ESLint; run ESLint (#1)

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    es2020: true,
+    jest: true,
+    node: true,
+  },
+  extends: [
+    'standard-with-typescript',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    project: 'tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    'comma-dangle': ['error', 'always-multiline'],
+    '@typescript-eslint/no-extraneous-class': 'off',
+  },
+}
