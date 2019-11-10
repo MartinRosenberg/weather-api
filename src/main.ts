a167a350bb368daf19c79acf1b1b38e06fec05a9
Martin Rosenberg <Martin.B.Rosenberg@gmail.com>
2019-11-10 10:53:33 -0700

    Switch Prettier/TSLint to ESLint; run ESLint

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+import { AppModule } from './app.module'
+
+async function bootstrap (): Promise<void> {
+  const app = await NestFactory.create(AppModule)
+  await app.listen(3000)
 }
-bootstrap();
+
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+bootstrap()
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,23 +1,24 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { Test, TestingModule } from '@nestjs/testing'
+import * as request from 'supertest'
+
+import { AppModule } from '../src/app.module'
 
 describe('AppController (e2e)', () => {
-  let app;
+  let app
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
-    }).compile();
+    }).compile()
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
+    app = moduleFixture.createNestApplication()
+    await app.init()
+  })
 
-  it('/ (GET)', () => {
+  it('/ (GET)', async () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
-  });
-});
+      .expect('Hello World!')
+  })
+})
